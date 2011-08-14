d990ae71b87ac14d52f69b0425346511f7ab6336
Moctezuma <gaussmath@gmail.com>
2011-08-14 15:19:36 -0500

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Flickr API
+require 'flickraw'
+
+# Initialize the rails application
+flickr-rails-ajax::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+flickr-rails-ajax::Application.config.secret_token = 'c6a86c01130d49f2fd2f71f3aa4740db174988f9348b89d6b8e8fbcfaf2595244133e5cbe21a8236c9c2f462220f512fba1da2a2599d4568737f6e3923ee67f8'
